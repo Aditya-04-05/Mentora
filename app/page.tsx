eae335487f45841fe419fc37d5374e164894a114
Aditya Garg <gargaditya59@gmail.com>
2026-03-26 18:55:04 +0530

    Implement Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,48 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { recentSessions, subjects } from './../constants/index';
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome To MENTORA</h1>
-      <Button>Hiiii</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color = "#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color = "#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="english"
+          duration={30}
+          color = "#ffdb6e"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
